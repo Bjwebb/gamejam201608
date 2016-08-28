2c3f344b5f9bd56ab954554add847b02237e8a47
Ben Webb <bjwebb67@googlemail.com>
2016-08-28 17:07:49 +0100

    Add levels

--- base.gd
+++ base.gd
@@ -7,14 +7,17 @@ extends Node2D
 var gear_scene
 var starting_gear
 var ending_gear
+var player_gear
+var time_done = 0
+var level
 
 var gears = []
-var score = 0
-var score_f = 0.0
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	level = get_node('/root/global').level
+	get_node('score').set_bbcode(str(level))
 	starting_gear = get_node('starting_gear')
 	starting_gear.starting_gear = true;
 	ending_gear = get_node('ending_gear')
@@ -24,21 +27,36 @@ func _ready():
 	gears.append(ending_gear)
 	new_gear()
 	set_process(true)
+	set_process_input(true)
 
 func new_gear():
-	var gear = gear_scene.instance();
-	gear.player = true
-	add_child(gear)
+	player_gear = gear_scene.instance();
+	player_gear.player = true
+	add_child(player_gear)
 	if rand_range(0,3) < 1:
-		gear.break_gear()
-	gears.append(gear)
+		player_gear.break_gear()
+	gears.append(player_gear)
+
+func _input(event):
+	if event.is_action_pressed('ui_select'):
+		get_tree().change_scene('res://base.tscn')
+	elif event.is_action_pressed('ui_page_up'):
+		get_node('/root/global').level += 1
+		get_tree().change_scene('res://base.tscn')
+	elif event.is_action_pressed('ui_page_down'):
+		get_node('/root/global').level -= 1
+		get_tree().change_scene('res://base.tscn')
 
 func _process(delta):
 	for gear in gears:
 		gear.rotv = 0
 	starting_gear.rotv = 1
 	starting_gear.tree_process()
+	
 	if ending_gear.rotv != 0:
-		score_f += delta * 10
-		score = int(score_f)
-		get_node('score').set_bbcode(str(score))
+		time_done += delta
+		if time_done > 1.5:
+			get_node('/root/global').level += 1
+			get_tree().change_scene('res://base.tscn')
+	else:
+		time_done = 0
